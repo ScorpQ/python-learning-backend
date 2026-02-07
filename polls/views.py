c04b8e4d987860d237f315d1ffc399e457ac9290
ScorpQ <cansinlale01@gmail.com>
2026-02-07 22:08:44 +0300

    feat: migration testing

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,8 @@
 from unittest import loader
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.db.models import F
 from .models import Question
 from .models import Choice
 
@@ -10,13 +12,30 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    print("question_id %s" % question_id, flush=True)
     detail_question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/details.html", {"question": detail_question})
 
-
 def result(request, question_id):
-    return HttpResponse("You're looking at the results of question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    print("ehehehehehehe", flush=True)
+    print(request.POST, flush=True)
+    try:
+        selected_choice = Choice.objects.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/details.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
